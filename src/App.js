e41a1e375e10d3ee41cd8deedb985dcc71a4ff97
Mark <mrigney@yozu.co.uk>
2019-10-02 09:10:20 +0100

    First Remote Commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import Recipe from './Recipe';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	const APP_ID = "88966184";
+	const APP_KEY = "8583c11c70bb4b734b3b037d141c146c";
+  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}`;
+  //Using a CORS proxy to get around CORS errors
+	const proxyurl = "https://cors-anywhere.herokuapp.com/";
+
+	const [recipes, setRecipes] = useState([]);
+
+	useEffect(() => {
+		getRecipes();
+	}, []);
+
+	const getRecipes = async () => {
+		//const response = await fetch(proxyurl+exampleReq);
+		const data = await response.json();
+		setRecipes(data.hits);
+	};
+
+	return (
+		<div className="App">
+			<form className="search-form">
+				<input type="text" className="search-bar" />
+				<button className="search-button" type="submit">
+					Search
+				</button>
+			</form>
+		</div>
+	);
+};
 
 export default App;
---
+++ src/Recipe.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Recipe = () => {
+	return (
+		<div>
+			<h1>Ttile</h1>
+			<p>Calories</p>
+			<img src="" alt="" />
+		</div>
+	);
+};
+
+export default Recipe;
